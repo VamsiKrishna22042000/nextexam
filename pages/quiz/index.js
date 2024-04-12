877c624e065c95ee3f4867d1438d768b09399038
VamsiKrishna22042000 <vamsi45678901@gmail.com>
2024-04-12 14:12:57 +0530

    shared domains

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,23 +1,38 @@
 import { useEffect } from "react";
 import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
 const Quiz = () => {
+  const router = useRouter();
+
   useEffect(() => {
     const receiveMessage = (event) => {
       // Ensure that the message is coming from a trusted source
-      // if (event.origin !== 'https://your-react-app-url.com') return;
+      if (event.origin !== "https://exam-userside.vercel.app") return;
       // Check the message type
       if (event.data.type === "login") {
         // Access the data sent from the iframe
         const { jwt_userID, userToken, jwt_firstName, jwt_lastName } =
           event.data;
         // Do something with the received data, for example, set cookies in the Next.js app
-        Cookies.set("jwt_userID", jwt_userID, { expires: 7 });
-        Cookies.set("userToken", userToken, { expires: 7 });
-        Cookies.set("jwt_firstName", jwt_firstName, { expires: 7 });
-        Cookies.set("jwt_lastName", jwt_lastName, { expires: 7 });
+        Cookies.set("jwt_userID", jwt_userID, {
+          expires: 7,
+          domain: ".vercel.app",
+        });
+        Cookies.set("userToken", userToken, {
+          expires: 7,
+          domain: ".vercel.app",
+        });
+        Cookies.set("jwt_firstName", jwt_firstName, {
+          expires: 7,
+          domain: ".vercel.app",
+        });
+        Cookies.set("jwt_lastName", jwt_lastName, {
+          expires: 7,
+          domain: ".vercel.app",
+        });
         // Optionally, you can also navigate or perform other actions in your Next.js app
-        // For example:
-        // router.push("/");
+        router.push("/");
       }
     };
     // Add an event listener to listen for messages from the iframe
@@ -31,8 +46,8 @@ const Quiz = () => {
   return (
     <div
       style={{
-        height: "100lvh",
-        width: "100lvw",
+        height: "100vh",
+        width: "100vw",
         marginTop: "7%",
       }}
     >
